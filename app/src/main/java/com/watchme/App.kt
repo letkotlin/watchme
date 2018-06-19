8c8f5450cf5473bce5151d9c06afd39d5e062cc2
kws <coreanim@gmail.com>
2018-06-19 15:55:29 +0900

    add base lib

---
+++ app/src/main/java/com/watchme/App.kt
@@ -0,0 +1,10 @@
+package com.watchme
+
+import android.app.Application
+
+/**
+ * Created by kws on 2017. 11. 22..
+ */
+class App : Application() {
+
+}
---
+++ app/src/main/java/com/watchme/view/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.watchme.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.watchme.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
